409727344ede759a25845ed3a9285d2918a5733e
Liau Jian Jie <liaujianjie@gmail.com>
2021-11-07 19:17:01 +0800

    Boostrap Tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Liau Jian Jie</title>
         <meta
@@ -15,21 +13,21 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Liau Jian Jie</h1>
+      <main>
+        <h1>Liau Jian Jie</h1>
 
-        <p className={styles.description}>
+        <p>
           Hello, I'm Jian Jie—a software engineer passionate about product and
           performance, I create thoughtful and performant software experiences
           through code.
         </p>
 
-        <p className={styles.description}>
+        <p>
           Currently, I'm working on <a href="https://mobbin.design">Mobbin</a>{" "}
           out of Singapore.
         </p>
 
-        <p className={styles.description}>
+        <p>
           Here are some places you can find me:
           <ul>
             <li>
@@ -44,13 +42,13 @@ const Home: NextPage = () => {
           </ul>
         </p>
 
-        <p className={styles.description}>
+        <p>
           Feel free to reach me via email at:
           <code>jianjie[at]mobbin.design</code>
         </p>
 
         {/* TODO: add PGP public key */}
-        {/* <p className={styles.description}>
+        {/* <p >
           Other stuff:
           <ul>
             <li>PGP public key</li>
