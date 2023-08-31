30f3aa29498f92cf0275ea3b74fe540052997aa3
Liau Jian Jie <jianjie@mobbin.com>
2023-09-01 00:48:40 +0800

    Update copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
         <title>Liau Jian Jie</title>
         <meta
           name="description"
-          content="I’m a software engineer passionate about product and performance, I create thoughtful and performant software experiences."
+          content="I’m passionate about creating value for the world through crafting thoughtful and performant software."
         />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
@@ -18,46 +18,34 @@ export default function HomePage() {
           <h1 className="font-black text-4xl mb-6">Hi, I’m Jian Jie.</h1>
 
           <h2 className="font-semibold text-2xl">
-            I’m a software engineer passionate about product and performance, I create thoughtful
-            and performant software experiences.
+            I’m passionate about creating value for the world through crafting thoughtful and
+            performant software.
           </h2>
         </section>
 
         <section className="mb-10">
           <p className="font-medium text-xl mb-4">
-            Currently, I’m leading the engineering team over at{" "}
-            <a href="https://mobbin.com">Mobbin</a>.
+            Currently, I’m the CTO and co-founder of <a href="https://mobbin.com">Mobbin</a>.
           </p>
 
           <p className="font-medium text-xl mb-4">
-            My software-oriented interests include machine learning, human-computer interactions
-            (HCI), conflict-free replicated data types (CRDT), and web technologies. I get excited
-            about creating value through building software.
+            My software-oriented interests include machine learning, human-computer interactions,
+            conflict-free replicated data types, and web technologies. I get excited about creating
+            value through building software.
           </p>
 
           <p className="font-medium text-xl mb-4">
             In my free time, I consume content from random topics from semiconductor fabrication to
-            public policy.
+            public policy. I am currently also learning Malay.
           </p>
 
           <p className="font-medium text-xl mb-4">
             You can find me on <a href="https://github.com/liaujianjie">GitHub</a>,{" "}
-            <a href="https://twitter.com/liaujianjie">Twitter</a>,{" "}
-            <a rel="me" href="https://mas.to/@jianjie">
-              Mastodon
-            </a>
-            , or as <code>liaujianjie</code> on other corners of the internet. You can also reach me
-            via email at <a href="mailto:jianjie@mobbin.com">jianjie@mobbin.com</a>.
+            <a href="https://twitter.com/liaujianjie">Twitter</a>, or as <code>liaujianjie</code> on
+            other corners of the internet. You can also reach me via email at{" "}
+            <a href="mailto:jianjie@mobbin.com">jianjie@mobbin.com</a>.
           </p>
         </section>
-
-        {/* <footer className="text-gray-500">
-          <ul>
-            <li>
-              <a href="/pgp">PGP Public Key</a>
-            </li>
-          </ul>
-        </footer> */}
       </main>
     </div>
   );
